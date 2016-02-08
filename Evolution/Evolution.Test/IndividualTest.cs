18e8f9db532e92683eec0a664707acac3c18fe9d
Bruno Garate <bruno.garate@gmail.com>
2016-02-08 15:29:17 -0300

    - Added untracked files

---
+++ Evolution/Evolution.Test/IndividualTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using Singular.Evolution.Core;
+using Singular.Evolution.Genes;
+using Singular.Evolution.Genotypes;
+
+namespace Evolution.Test
+{
+    [TestFixture]
+    public class IndividualTest
+    {
+        [Test]
+        public void TestIndividualConstructors()
+        {
+            ListGenotype<FloatGene> genotype1 = new ListGenotype<FloatGene>(new []{new FloatGene(1) });
+            ListGenotype<FloatGene> genotype2 = new ListGenotype<FloatGene>(new[] {new FloatGene(2)});
+
+            Individual<ListGenotype<FloatGene>, int> individual1 = new Individual<ListGenotype<FloatGene>, int>(genotype1);
+            Individual<ListGenotype<FloatGene>, int> individual2 = new Individual<ListGenotype<FloatGene>, int>(genotype2,30);
+
+            Assert.AreEqual(1,individual1.Genotype.Count);
+            Assert.AreEqual(1, individual2.Genotype.Count);
+
+            Assert.False(individual1.HasFitnessAssigned);
+            Assert.True(individual2.HasFitnessAssigned);
+
+            Assert.Throws<Exception>(() => { var i = individual1.Fitness; });
+            Assert.AreEqual(30, individual2.Fitness);
+
+            var genotype3 = individual1.Genotype;
+            Assert.AreSame(individual1.Genotype,genotype3);
+
+            var individuals =
+                Individual<ListGenotype<FloatGene>, int>.FromGenotypes(new[] {genotype1, genotype2, genotype3});
+
+            Assert.AreSame(individuals[0].Genotype,genotype1);
+            Assert.AreSame(individuals[1].Genotype, genotype2);
+            Assert.AreSame(individuals[2].Genotype, genotype3);
+        }
+    }
+}
